f0007f963376a8bad0aa9f7a4f3a9e47c5a1490d
DilipLovevanshi <dilip@aperasoftwares.com>
2025-05-13 16:31:34 +0530

    designed refer screen

---
+++ src/app/(admin)/(others-pages)/referral/page.tsx
@@ -0,0 +1,51 @@
+import ReferralFromSection from "@/components/ReferralFromSection";
+import ServiceCard from "@/components/ServiceCard";
+import { SERVICES } from "@/data/services";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: "Next.js Blank Page | TailAdmin - Next.js Dashboard Template",
+    description: "This is Next.js Blank Page TailAdmin Dashboard Template",
+};
+
+export default function BlankPage() {
+    return (
+        <div>
+            <div className="mb-6">
+                <h1
+                    className="text-3xl font-semibold text-gray-800 dark:text-white/90"
+                    x-text="pageName"
+                >
+                    Catherine Chen       </h1>
+                <div className="text-lg text-[#1F1C3B]">
+                    Check out my top picks and refer someone who needs them!
+                </div>
+            </div>
+            <div className="w-full max-w-[1500px] grid grid-cols-1  lg:grid-cols-3 justify-center gap-10  lg:gap-6 my-4">
+                {SERVICES && SERVICES?.length > 0 ? (
+                    SERVICES?.map((serviceItems: any, index: number) => (
+                        <ServiceCard
+                            key={serviceItems?.id}
+                            title={serviceItems?.title}
+                            points={serviceItems?.servicesPoints}
+                            images={serviceItems?.images}
+                        />
+                    ))
+                ) : (
+                    <div></div>
+                )}
+            </div>
+
+            <div className="mb-4 mt-10">
+                <h1 className="text-3xl font-semibold text-gray-800 dark:text-white/90"
+                    x-text="pageName">
+                    Referral Submission Form              
+               </h1>
+
+            </div>
+            <ReferralFromSection />
+
+        </div>
+    );
+}
---
+++ src/data/services.ts
@@ -0,0 +1,48 @@
+export const SERVICES = [
+  {
+    id: "101",
+    title: "Pool Solar",
+    servicesPoints: [
+      "Low upfront costs",
+      "No gas or electric costs",
+      "Raises water temps 10° to 15°",
+    ],
+    images: [
+      "/assets/images/service-image-1.png",
+      "/assets/images/service-image-2.png",
+      "/assets/images/service-image-3.png",
+    ],
+    link: "/",
+  },
+  {
+    id: "102",
+    title: "Pool Solar",
+    servicesPoints: [
+      "American-made Westinghouse Quality",
+      " Industrial Grade for the Hardest Water",
+      " Best-in-Class Lifetime Warranty",
+    ],
+    images: [
+      "/assets/images/service-image-2.png",
+      "/assets/images/service-image-1.png",
+
+      "/assets/images/service-image-3.png",
+    ],
+    link: "/",
+  },
+  {
+    id: "103",
+    title: "Photovoltaic (PV)",
+    servicesPoints: [
+      "12-Year Warranty",
+      " 1-Day Installation",
+      " Financing Available",
+    ],
+    images: [
+      "/assets/images/service-image-3.png",
+      "/assets/images/service-image-1.png",
+      "/assets/images/service-image-2.png",
+    ],
+    link: "/",
+  },
+];
