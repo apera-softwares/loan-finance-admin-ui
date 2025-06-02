5be5e54b9c7006abc966863e13ce7dd7b9927c90
tushar21 <tnikam21@gmail.com>
2025-06-02 23:18:39 +0530

    Created github action pipeline

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
+  // distDir: 'dist',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
